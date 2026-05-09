4fba1d51262afaa204e73c4a06d4648299282e24
sinemavci <sinem.avci@ekinokssoftware.com>
2026-05-09 16:44:53 +0300

    fix: add missing imports

--- lib/common/enums/offline_policy_action.dart
+++ lib/common/enums/offline_policy_action.dart
@@ -4,7 +4,7 @@ sealed class OfflinePolicyAction {
   const OfflinePolicyAction(this.identifier);
 
   static const queue      = QueuePolicyAction();
-  static const useCache   = UseCachePolicyAction();
+  static const useCache   = UseCacheOfflinePolicyAction();
   static const throwError = ThrowErrorPolicyAction();
   static RetryPolicyAction retry({required int retries}) =>
       RetryPolicyAction(retries: retries);
@@ -12,7 +12,7 @@ sealed class OfflinePolicyAction {
   static OfflinePolicyAction fromIdentifier(String identifier, {int? retries}) {
     return switch (identifier) {
       "QUEUE"       => const QueuePolicyAction(),
-      "USE_CACHE"   => const UseCachePolicyAction(),
+      "USE_CACHE"   => const UseCacheOfflinePolicyAction(),
       "RETRY"       => RetryPolicyAction(retries: retries ?? 1),
       "THROW_ERROR" => const ThrowErrorPolicyAction(),
       _             => throw ArgumentError("Unknown offline policy: $identifier"),
@@ -24,8 +24,8 @@ class QueuePolicyAction extends OfflinePolicyAction {
   const QueuePolicyAction() : super("QUEUE");
 }
 
-class UseCachePolicyAction extends OfflinePolicyAction {
-  const UseCachePolicyAction() : super("USE_CACHE");
+class UseCacheOfflinePolicyAction extends OfflinePolicyAction {
+  const UseCacheOfflinePolicyAction() : super("USE_CACHE");
 }
 
 class RetryPolicyAction extends OfflinePolicyAction {
--- lib/netra_flutter_plugin.dart
+++ lib/netra_flutter_plugin.dart
@@ -1,3 +1,11 @@
 library netra_flutter_plugin;
 
 export './common/netra_client.dart';
+
+export './common/models/request_body.dart';
+export './common/models/request_options.dart';
+export './common/models/response.dart';
+
+export './common/enums/converter_type.dart';
+export './common/enums/offline_policy_action.dart';
+export './common/enums/slow_network_policy_action.dart';
